a44cfac69c89711fb0a6f7868417d96acd952e2f
Claudio Junior <claudiodsesjunior@gmail.com>
2019-12-24 16:45:00 -0300

    password limitations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :surname, presence: true, length: { minimum: 3 }
+
+  PASSWORD_FORMAT = /\A
+  (?=.{8,})          # Must contain 8 or more characters
+  (?=.*\d)           # Must contain a digit
+  (?=.*[a-z])        # Must contain a lower case character
+  (?=.*[A-Z])        # Must contain an upper case character
+  (?=.*[[:^alnum:]]) # Must contain a symbol
+  /x
+
+  validates :password, presence: true, format: { with: PASSWORD_FORMAT }
+
   def destroy
     update_attributes(active: false) unless active
   end
