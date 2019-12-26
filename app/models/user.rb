98ef39312b8496e9b77e1dba72feae1243d7cae6
Claudio Junior <claudiodsesjunior@gmail.com>
2019-12-26 15:10:18 -0300

    password fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :surname, presence: true, length: { minimum: 3 }
 
   PASSWORD_FORMAT = /\A
-  (?=.{8,})          # Must contain 8 or more characters
+  (?=.{6,})          # Must contain 8 or more characters
   (?=.*\d)           # Must contain a digit
   (?=.*[a-z])        # Must contain a lower case character
   (?=.*[A-Z])        # Must contain an upper case character
