b2362c6822405351494b7bd48792543868e42fcd
Claudio Junior <claudiodsesjunior@gmail.com>
2019-12-26 16:26:44 -0300

    bug fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,21 @@
 class UsersController < ApplicationController
     def setActiveFlag
-      @user = User.find(params[:id])
+        @user = User.find(params[:id])
 
-    if @user.active 
-        if @user.update(:active => false)
-            flash[:success] = "User was successfully deactivated"
-            redirect_back(fallback_location: root_path)
+        if @user.active 
+            if @user.update_attribute(:active, false)
+                flash[:success] = "User was successfully deactivated"
+                redirect_to '/'
+            else
+                flash[:error] = "Something went wrong"
+            end
         else
-            flash[:error] = "Something went wrong"
-        end
-    else
-        if @user.update(:active => true)
-            flash[:success] = "User was successfully activated"
-            redirect_back(fallback_location: root_path)
-        else
-            flash[:error] = "Something went wrong"
+            if @user.update_attribute(:active, true)
+                flash[:success] = "User was successfully activated"
+                redirect_to '/'
+            else
+                flash[:error] = "Something went wrong"
+            end
         end
     end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  
   devise_scope :user do
     authenticated :user do
-      root "pages#home", as: :authenticated_root
+      root to: "pages#home", as: :authenticated_root
     end
 
-    unauthenticated do
-      root "devise/sessions#new", as: :unauthenticated_root
+    unauthenticated :user do
+      root to: "devise/sessions#new", as: :unauthenticated_root
     end
   end
   
-  match 'users/:id' => 'users#setActiveFlag', :via => :put, :as => :user_active_flag
+  match 'users/:id' => 'users#setActiveFlag', :via => :patch, :as => :user_active_flag
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
